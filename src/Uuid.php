4a8f279fa73ff96c42da3e004d6d7948449dae84
tito10047 <jozef@mostka.sk>
2025-02-13 15:05:41 +0100

    initial commit

---
+++ src/Uuid.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tito10047\Uuid;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+/**
+ * @link https://symfony.com/doc/current/bundles/best_practices.html
+ */
+class Uuid extends AbstractBundle
+{
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.php');
+    }
+}
